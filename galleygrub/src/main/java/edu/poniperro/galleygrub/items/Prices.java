4aab7609b33e824c8b6528bba67007d81e29799d
ErikPC <epardillo@cifpfbmoll.eu>
2022-02-17 18:24:28 +0100

    feat: crear clase prices

--- galleygrub/src/main/java/edu/poniperro/galleygrub/items/Prices.java
+++ galleygrub/src/main/java/edu/poniperro/galleygrub/items/Prices.java
@@ -4,20 +4,30 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Prices {
-    private final Map<String, Double> items = new HashMap<String, Double>(
-            Map.of(
-                    "Krabby Patty", 1.25,
-                    "Coral Bits", 1.00,
-                    "Kelp Rings", 1.50,
-                    "Golden Loaf", 2.00,
-                    "Seafoam Soda", 1.00,
-                    "cheese", 0.25,
-                    "sauce", 0.50,
-                    "medium", 0.25,
-                    "large", 0.50));
+    private static Map<String, Double> items;
 
-    public Map<String, Double> getItems() {
-        return this.items;
+    public Prices() {
+    }
+
+    public static void init_prices() {
+        items = new HashMap<String, Double>(
+                Map.of(
+                        "Krabby Patty", 1.25,
+                        "Coral Bits", 1.00,
+                        "Kelp Rings", 1.50,
+                        "Golden Loaf", 2.00,
+                        "Seafoam Soda", 1.00,
+                        "cheese", 0.25,
+                        "sauce", 0.50,
+                        "medium", 0.25,
+                        "large", 0.50));
+
+    }
+
+    public static void display() {
+        for (Map.Entry<String, Double> entry : items.entrySet()) {
+            System.out.println(entry);
+        }
     }
 
 }

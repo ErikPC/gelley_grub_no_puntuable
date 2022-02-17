935b237f0bb54efbb635bf85d873ae04955465fd
ErikPC <epardillo@cifpfbmoll.eu>
2022-02-17 20:04:33 +0100

    test: testear clase Prices

--- galleygrub/src/test/java/edu/poniperro/galleygrub/items/PricesTest.java
+++ galleygrub/src/test/java/edu/poniperro/galleygrub/items/PricesTest.java
@@ -1,10 +1,15 @@
 package edu.poniperro.galleygrub.items;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class PricesTest {
     @Test
     public void init_pricesTest() {
         Prices.init_prices();
+
+        assertEquals(4, Prices.getItems().size());
     }
+
 }

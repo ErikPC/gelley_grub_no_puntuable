dba4a078bf7be9d27ae36f074e6ddaff37e74ac9
ErikPC <epardillo@cifpfbmoll.eu>
2022-02-17 17:57:42 +0100

    feat: crear intefaz item

---
+++ galleygrub/src/main/java/edu/poniperro/galleygrub/items/Item.java
@@ -0,0 +1,58 @@
+package edu.poniperro.galleygrub.items;
+
+public class Item implements Product {
+
+    String name;
+    Double price;
+    String extra = "null";
+
+    Item(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    Item(String name, double price, String extra) {
+        this.name = name;
+        this.price = price;
+        this.extra = extra;
+    }
+
+    public String name() {
+        return this.name;
+    }
+
+    public Double price() {
+        return this.price;
+    }
+
+    public String extra() {
+        return this.extra;
+    }
+
+    public Boolean isRegular() {
+        if (extra() == "null") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "nombre : " + name();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Item) {
+            return this.name.equals(((Item) obj).name);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode();
+    }
+
+}

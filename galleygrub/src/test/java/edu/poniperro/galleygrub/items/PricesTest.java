e0e4e5b8ce06cebfa1e8d7aec021e557da8070b9
ErikPC <epardillo@cifpfbmoll.eu>
2022-02-17 19:52:19 +0100

    test: lanzar modulo init para ver si rompe

---
+++ galleygrub/src/test/java/edu/poniperro/galleygrub/items/PricesTest.java
@@ -0,0 +1,10 @@
+package edu.poniperro.galleygrub.items;
+
+import org.junit.Test;
+
+public class PricesTest {
+    @Test
+    public void init_pricesTest() {
+        Prices.init_prices();
+    }
+}

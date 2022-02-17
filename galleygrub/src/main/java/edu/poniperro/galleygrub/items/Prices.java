53996a68192e18d0dd3c518d816e68ad44af1dc5
ErikPC <epardillo@cifpfbmoll.eu>
2022-02-17 19:51:26 +0100

    fix: añadir solo extras

--- galleygrub/src/main/java/edu/poniperro/galleygrub/items/Prices.java
+++ galleygrub/src/main/java/edu/poniperro/galleygrub/items/Prices.java
@@ -12,11 +12,6 @@ public class Prices {
     public static void init_prices() {
         items = new HashMap<String, Double>(
                 Map.of(
-                        "Krabby Patty", 1.25,
-                        "Coral Bits", 1.00,
-                        "Kelp Rings", 1.50,
-                        "Golden Loaf", 2.00,
-                        "Seafoam Soda", 1.00,
                         "cheese", 0.25,
                         "sauce", 0.50,
                         "medium", 0.25,
@@ -26,7 +21,7 @@ public class Prices {
 
     public static void display() {
         for (Map.Entry<String, Double> entry : items.entrySet()) {
-            System.out.println(entry);
+            System.out.println("\t" + entry);
         }
     }
 

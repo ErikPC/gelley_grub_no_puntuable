c2a8e163b076b421eb08473894760ad7145d605a
ErikPC <epardillo@cifpfbmoll.eu>
2022-03-14 23:53:28 +0100

    feat: añadir metodo set y metodo abstracto acabando los atributos

--- galleygrub/src/main/java/edu/poniperro/galleygrub/extras/Extras.java
+++ galleygrub/src/main/java/edu/poniperro/galleygrub/extras/Extras.java
@@ -1,10 +1,22 @@
 package edu.poniperro.galleygrub.extras;
 
-public class Extras {
+import java.util.Optional;
+
+import edu.poniperro.galleygrub.order.Comanda;
+
+public abstract class Extras {
     static final String CHEESE = "cheese";
     static final String SAUCE = "sauce";
     static final String SIZE_LARGE = "large";
+    String extraProduct;
+    Optional<Extras> nextExtra = Optional.empty();
 
     public Extras() {
     };
+
+    public void setNextExtra(Extras extra) {
+        this.nextExtra = Optional.of(extra);
+    }
+
+    public abstract void sumExtras(Comanda comanda);
 }

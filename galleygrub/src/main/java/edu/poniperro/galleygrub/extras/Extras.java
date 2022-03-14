d7adbc5387593932d436373d9eefed52bc257bcf
ErikPC <epardillo@cifpfbmoll.eu>
2022-03-14 19:54:52 +0100

    feat: crear clase extras sin metodos

---
+++ galleygrub/src/main/java/edu/poniperro/galleygrub/extras/Extras.java
@@ -0,0 +1,10 @@
+package edu.poniperro.galleygrub.extras;
+
+public class Extras {
+    static final String CHEESE = "cheese";
+    static final String SAUCE = "sauce";
+    static final String SIZE_LARGE = "large";
+
+    public Extras() {
+    };
+}

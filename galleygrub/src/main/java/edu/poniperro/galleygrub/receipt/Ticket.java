bf9018cb01f9846b19f9d44a70338ad7aedab5ba
ErikPC <epardillo@cifpfbmoll.eu>
2022-03-15 02:06:47 +0100

    fix: descomentar los metodos de los extras

--- galleygrub/src/main/java/edu/poniperro/galleygrub/receipt/Ticket.java
+++ galleygrub/src/main/java/edu/poniperro/galleygrub/receipt/Ticket.java
@@ -1,15 +1,18 @@
 package edu.poniperro.galleygrub.receipt;
 
+import edu.poniperro.galleygrub.extras.Extras;
 import edu.poniperro.galleygrub.order.Comanda;
 
 public interface Ticket {
     public Comanda getOrder();
 
-    // public void setChain(Extra extra);
-    // public Extra getChain();
+    public void setChain(Extras extra);
+
+    public Extras getChain();
+
     public Double total();
 
-    // public void sumExtrasCharge();
+    public void sumExtrasCharge();
 
     public void print();
 }

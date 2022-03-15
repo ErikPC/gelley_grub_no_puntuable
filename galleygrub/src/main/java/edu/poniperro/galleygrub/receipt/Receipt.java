2c551f1e6f452fae57e3286ace050cf05878cc4c
ErikPC <epardillo@cifpfbmoll.eu>
2022-03-15 02:07:06 +0100

    feat: añadir metodos de extras

--- galleygrub/src/main/java/edu/poniperro/galleygrub/receipt/Receipt.java
+++ galleygrub/src/main/java/edu/poniperro/galleygrub/receipt/Receipt.java
@@ -1,11 +1,13 @@
 package edu.poniperro.galleygrub.receipt;
 
+import edu.poniperro.galleygrub.extras.Extras;
 import edu.poniperro.galleygrub.order.Comanda;
 
 public class Receipt implements Ticket {
 
     private Double total = 0.00;
     private Comanda order;
+    private Extras firstExtra;
 
     public Receipt(Comanda comanda) {
         this.order = comanda;
@@ -18,12 +20,33 @@ public class Receipt implements Ticket {
 
     @Override
     public Double total() {
+        if (firstExtra != null) {
+            sumExtrasCharge();
+        }
+
         return this.total;
+    }
+
+    @Override
+    public void setChain(Extras extra) {
+        this.firstExtra = extra;
+    }
+
+    @Override
+    public Extras getChain() {
+        return this.firstExtra;
+    }
+
+    @Override
+    public void sumExtrasCharge() {
+        firstExtra.sumExtras(getOrder());
+        this.total = getOrder().getTotal();
+
     }
 
     @Override
     public void print() {
         getOrder().display();
-        System.out.println("\tTOTAL--------> " + String.format("%.2f", total()) + "$");
+        System.out.println("\tTOTAL--------> " + String.format("%.2f", this.total) + "$");
     }
 }
